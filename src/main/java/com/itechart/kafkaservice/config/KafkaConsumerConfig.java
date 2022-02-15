3678c076ba7709fe68834b46a52fb7cfc90848dc
Anton Belousov <antony.belousov02@gmail.com>
2022-02-15 10:13:16 +0300

    removing comments

--- src/main/java/com/itechart/kafkaservice/config/KafkaConsumerConfig.java
+++ src/main/java/com/itechart/kafkaservice/config/KafkaConsumerConfig.java
@@ -7,11 +7,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
